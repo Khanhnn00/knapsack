e6346333c017d00212afecb5a683afa456e43e36
Khanhnn00 <18520901@gm.uit.edu.vn>
2021-02-04 00:31:56 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# knapsack
+# The repository includes approaches to solve knapsack 0/1 problem. </br>
+
+Source code contained in **source.py**.</br>
+
+<dl>
+  <dt>In the experiments folder, there are:</dt>
+  <dd>bottomup_data.csv: Experiment on time complexity of bottom-up approach.</dd>
+  <dd>topdown_data.csv: Experiment on time complexity of top-down approach.</dd>
+  <dd>bruteforce.csv: Experiment on time complexity of bruteforce approach.</dd>
+  <dd>btm_optimized_memory_compare.csv: Experiment on memory complexity of bottom-up approach with optimization.</br>
+    On the left side, measuring memory allocated with n and W variables both changed</br>
+    On the right side, measuring memory allocated with n changed and W unchaged.</dd>
+</dl>
+
+**Input folder** contains inputs for test cases.
+**Output folder** contains outputs of according inputs given in **input folder**
+
+**cs112.ipynb**: file is used to predict runtime of each approach in different time complexity functions, using linear regression.
+
+    
--- sauce.py
+++ sauce.py
@@ -7,9 +7,11 @@ import time
 import random
 import csv
 
-# sys.setrecursionlimit(10**6)
-
 def htop(snapshot, key_type='lineno', limit=3):
+    '''
+    This function is used for measuring the memory usage of any certain function
+    It prints the top 3 lines that consume most memory and return the total memory that function allocated
+    '''
     snapshot = snapshot.filter_traces((
         tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
         tracemalloc.Filter(False, "<unknown>"),
@@ -35,32 +37,23 @@ def htop(snapshot, key_type='lineno', limit=3):
     print("Total allocated size: %.1f KiB" % (total / 1024))
     return total/1024
 
-def printComponents(memoi, capacity, items,values, weights):
-    temp = 0
-    components = [0] * items
-    w = capacity
-    res = memoi[items][w]    #0
-    for i in range(items, 0, -1): 
-        if res < 0:
-            break
-        if res != memoi[i-1][w]:
-            components[i-1] = 1
-            res -= values[i-1]
-            w = w - weights[i-1]
-            temp += values[i-1]
-        else:
-            continue
-    # print('test: {}'.format(temp))
-    return components
-
-def f(number, capacity, weights, values): #brute force
+def bruteForce(number, capacity, weights, values): 
+    '''
+    bruteForce approach to solve knapsack 0/1 problem
+    Input (from left to right): 
+        number of items
+        the capacity of the bag
+        array contains weight of each item
+        array contains value of each item
+    Output:
+        the maximum value that can be taken
+    '''  
     weight_cost = []
     for kas in range(len(weights)):
         kass = (weights[kas], values[kas])
         weight_cost.append(kass)
     best_cost = None
     best_combination = []
-    # generating combinations by all ways: C by 1 from n, C by 2 from n, ...
     for way in range(number):
         for comb in combinations(weight_cost, way + 1):
             weight = sum([wc[0] for wc in comb])
@@ -72,8 +65,17 @@ def f(number, capacity, weights, values): #brute force
                     best_combination[weight_cost.index(wc)] = 1
     return best_cost
 
-
-def f(num, capacity, w, c):   #top-down approach
+def topDown(num, capacity, w, c): 
+    '''
+    top-down approach to solve knapsack 0/1 problem
+    Input (from left to right): 
+        number of items
+        the capacity of the bag
+        array contains weight of each item
+        array contains value of each item
+    Output:
+        the maximum value that can be taken
+    '''  
     #base case
     if num == 0 or capacity == 0:
         return 0
@@ -88,84 +90,87 @@ def f(num, capacity, w, c):   #top-down approach
         memoi[num][capacity] = max(c[num-1] + f(num-1, capacity-w[num-1], w,c), f(num-1, capacity, w,c))
         return memoi[num][capacity]
 
-# def f(num, capacity, w, c): #btm up
-    # # memoi = [[0 for x in range(capacity + 1)] for x in range(num + 1)] #dont fucking need this
+def btmUp(num, capacity, w, c):
+    '''
+    bottom-up approach to solve knapsack 0/1 problem
+    Input (from left to right): 
+        number of items
+        the capacity of the bag
+        array contains weight of each item
+        array contains value of each item
+    Output:
+        the maximum value that can be taken
+    '''
+    memoi = [[0 for x in range(capacity + 1)] for x in range(num + 1)] 
   
-#     for i in range(num + 1): 
-#         for j in range(capacity + 1): 
-#             if i == 0 or j == 0: 
-#                 memoi[i][j] = 0
-#             elif w[i-1] <= j:
-#                 memoi[i][j] = max(c[i-1] 
-#                           + memoi[i-1][j-w[i-1]],   
-#                               memoi[i-1][j]) 
-#             else: 
-#                 memoi[i][j] = memoi[i-1][j]
-#     # print(memoi)
-#     # tempp = []
-#     # for i in range(num+1):
-#     #     tempp.append(memoi[i][capacity])
-#     # print(tempp)
-#     # print(printComponents(memoi, capacity, num, c, w))
-#     return memoi[num][capacity] 
+    for i in range(num + 1): 
+        for j in range(capacity + 1): 
+            if i == 0 or j == 0: 
+                memoi[i][j] = 0
+            elif w[i-1] <= j: 
+                memoi[i][j] = max(c[i-1] 
+                          + memoi[i-1][j-w[i-1]],   
+                              memoi[i-1][j]) 
+            else: 
+                memoi[i][j] = memoi[i-1][j] 
+  
+    return memoi[num][capacity]
 
-# tracemalloc.start()
-
-global nums, capacity, weights, values  #dont fvcking comment this bobo.
-weights = []
-values = []
-with open('./input3.txt', 'r') as file:
-    for line in file:
-        nums = line.split()
-        items = int(nums[0])
-        capacity = int(nums[1])
-        for i in range(2, len(nums)):
-            if i % 2 == 0:
-                weights.append(int(nums[i]))
-            else:
-                values.append(int(nums[i]))
-file.close()
-'''
-Above is reading input from txt file. Instead u can set the value by uncommenting the following lines.
-And ofc comment the above lines.
-'''
-# capacity = 7
-# values= [1,4,5,7]
-# weights = [1,3,4,5]
-# items = 4
-# print(values)
-memoi = [[0 for x in range(capacity + 1)] for x in range(items + 1)]
-print(f(items, capacity, weights, values))
-
+def f(num, capacity, w, v):  #optimized
+    '''
+    bottom-up approach which is optimized to solve knapsack 0/1 problem
+    Input (from left to right): 
+        number of items
+        the capacity of the bag
+        array contains weight of each item
+        array contains value of each item
+    Output:
+        the maximum value that can be taken
+    '''
+    memoi = [[0 for x in range(capacity + 1)]  
+              for x in range(2)]
+    for i in range(num): 
+        j = 0   
+        while j < capacity:
+            j += 1
+            if w[i] <= j:
+                memoi[~(i&1)][j] = max(v[i] + memoi[(i&1)][j - w[i]], memoi[(i&1)][j]) 
+            else: 
+                memoi[~(i&1)][j] = memoi[(i&1)][j] 
+    return memoi
 
 '''
-below is using for generating n and W for testing experiment results.
+Below is the code we use to conduct experiment on proving time and memory
+complexity of each approach
 '''
-# with open('bruteforce.csv', mode='a') as csvfile:
-#     writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     for i in range(1, 1000, 1):
-#         tracemalloc.start()
-#         weights = []
-#         values = []
-#         items = i
-#         # weights = random(range(0, 70), i)
-#         for j in range(i):
-#             tmp = random.randint(0, 70)
-#             weights.append(tmp)
+with open('btm_opt_2.csv', mode='w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for i in range(100, 1000, 10):
+        weights = []
+        values = []
+        items = i
+        # weights = random(range(0, 70), i)
+        for j in range(i):
+            tmp = random.randint(0, 70)
+            weights.append(tmp)
 
-#         for j in range(i):
-#             tmp = random.randint(50, 300)
-#             values.append(tmp)
-#         capacity = 400
-#         start = time.time()
-#         for i in range(100):
-#             f(items, capacity, weights, values)
-#         print(f(items, capacity, weights, values))
-#         end = time.time()
-#         print('Total time: {}'.format(end-start))
-#         snapshot = tracemalloc.take_snapshot()
-#         mem = htop(snapshot)
-#         writer.writerow([str(items), str(capacity), str(end-start), str(mem)])
-#         if end-start > 600:
-#             break
-    
+        for j in range(i):
+            tmp = random.randint(50, 300)
+            values.append(tmp)
+        capacity = 1000
+        start = time.time()
+        tracemalloc.start()
+        temp = []
+        for k in range(30):
+            res = f(items, capacity, weights, values)
+            temp.append(res)
+        a = f(items, capacity, weights, values)
+        end = time.time()
+        print('Total time: {}'.format(end-start))
+        snapshot = tracemalloc.take_snapshot()
+        tracemalloc.stop()
+        mem = htop(snapshot)
+        writer.writerow([str(items), str(capacity), str(end-start), str(mem)])
+        del(temp)
+        if i>500 or int(end-start) > 300:
+            break
